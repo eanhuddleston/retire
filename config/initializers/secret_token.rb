54c5af917426f4d2f2d72320401643d4188961e6
Ean Huddleston <ean.huddleston@gmail.com>
2014-09-27 16:35:29 -0700

    Run Rails new project generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Retire::Application.config.secret_token = 'ee773a9c93d5724bd9304f1d7e99686dbdcc8b8d9f2922cfd5165edb4ed008377d3a3b3c9d461ec205b97e11e4aaab7d1038f65774a8126822de6b9887c0125f'
